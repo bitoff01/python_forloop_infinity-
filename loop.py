1f8c83b47157698af507cba7bed21d51d33cb2d7
Rajit Aryan <69615631+bitoff01@users.noreply.github.com>
2020-12-19 15:05:11 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,8 @@
+#taking input for the list in which we are going to add the out comes.
+l=list(eval(input()))
+#implementing the for loop
+for i in l:
+    i= i+5
+    l.append(i)
+    print(i)
+#as you run these lines you will see that the value of i will increse many times
