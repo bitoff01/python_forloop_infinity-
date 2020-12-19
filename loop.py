fb23c6d06dafc37f377c0738128ffd6d9ec5b62a
Rajit Aryan <69615631+bitoff01@users.noreply.github.com>
2020-12-19 21:08:31 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -3,5 +3,5 @@ l=list(eval(input()))
 #implementing the for loop
 for i in l:
     l.append(i+5)
-    print(i)
+    print(i)#or print(l)
 #as you run these lines you will see that the value of i will increse many times
