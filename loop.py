b23544f4575d0e11f74ec76ec92c7ae5f5b011bf
Rajit Aryan <69615631+bitoff01@users.noreply.github.com>
2020-12-20 14:33:23 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,7 +1,12 @@
+"""
+Created on Sat Dec 19 14:37:20 2020
+
+@author: Rajit aryan
+"""
 #taking input for the list in which we are going to add the out comes.
 l=list(eval(input()))
 #implementing the for loop
 for i in l:
     l.append(i+5)
-    print(i)#or print(l)
-#as you run these lines you will see that the value of i will increse many times
+    print(i
+#as you run these lines you will see that the value of i will increse ever time it get printed
